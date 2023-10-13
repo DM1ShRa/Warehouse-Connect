05873b24657f568cb98dcb298b1535073694b4b6
Darash Mishra <darash.mishra@gmail.com>
2023-10-13 20:20:21 +0530

    Location on Map

--- components/GoogleMapsView.jsx
+++ components/GoogleMapsView.jsx
@@ -1,47 +1,52 @@
 "use client"
 
 import { UserLocationContext } from "@context/UserLocationContext"
-import { GoogleMap,LoadScript, MarkerF } from "@react-google-maps/api"
+import { GoogleMap, LoadScript, MarkerF } from "@react-google-maps/api"
 import { useContext } from "react"
+import locations from "@seeds/cities"
 
-function GoogleMapsView() {
-    const {userLocation , setUserLocation} = useContext(UserLocationContext);
+function GoogleMapsView({ post }) {
+    const { userLocation, setUserLocation } = useContext(UserLocationContext);
 
-    const containerStyle={
+    const containerStyle = {
         width: '100%',
-        height:'70vh'
+        height: '70vh'
     }
-    const cordinate={
-        lat: 19.032801 ,
-        lng:72.896355
+    const cordinate = {
+        lat: 19.032801,
+        lng: 72.896355
     }
     console.log(userLocation)
-  return (
-    <div>
-        <LoadScript
-        googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_API_KEY}
-        mapIds={['37a8b8b8bc0b1934']}
-        >
-            <GoogleMap
-                mapContainerStyle={containerStyle}
-                center={userLocation}
-                options={{mapId:'37a8b8b8bc0b1934'}}
-                zoom={12}
+
+    return (
+        <div>
+            <LoadScript
+                googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_API_KEY}
+                mapIds={['37a8b8b8bc0b1934']}
             >
-                <MarkerF
-                position={userLocation}
-                icon={{
-                    url:'./wh.png',
-                    scaledSize:{
-                        width : 50,
-                        height : 50
-                    }
-                }}
-                />
-            </GoogleMap>
-        </LoadScript>
-    </div>
-  )
+                <GoogleMap
+                    mapContainerStyle={containerStyle}
+                    center={userLocation}
+                    options={{ mapId: '37a8b8b8bc0b1934' }}
+                    zoom={12}
+                >
+                    {locations.map((location) => (
+                        <MarkerF
+                            position={location}
+                            icon={{
+                                url: './wh.png',
+                                scaledSize: {
+                                    width: 50,
+                                    height: 50
+                                }
+                            }}
+                        />
+
+                    ))}
+                </GoogleMap>
+            </LoadScript>
+        </div>
+    )
 }
 
 export default GoogleMapsView
---
+++ seeds/cities.js
@@ -0,0 +1,7 @@
+var locations = [
+  { lat: 19.076, lng: 72.8777 },
+  { lat: 19.0745, lng: 72.9978 },
+  { lat: 19.0522, lng: 72.9005 },
+];
+
+export default locations;
